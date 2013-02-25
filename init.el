55415d4bc59b1ef2a8cd24dfdd223b301f86b216
Chris Perkins and Jake Pearson <pair+cp+jp@rallydev.com>
2013-02-25 12:19:44 -0700

    Buffers are saved when frame is deactivated

--- init.el
+++ init.el
@@ -396,6 +396,21 @@
   (insert (string-as-multibyte "ಠ_ಠ")))
 (global-set-key (kbd "C-x _") 'look-of-disapproval)
 
+;; automatically save buffers associated with files on buffer switch                                                                                                                                      
+;; and on windows switch                                                                                                                                                                                  
+(defadvice switch-to-buffer (before save-buffer-now activate)                                                                                                                                             
+  (when buffer-file-name (save-buffer)))                                                                                                                                                                  
+(defadvice other-window (before other-window-now activate)                                                                                                                                                
+  (when buffer-file-name (save-buffer)))                                                                                                                                                                  
+(defadvice windmove-up (before other-window-now activate)                                                                                                                                                 
+  (when buffer-file-name (save-buffer)))                                                                                                                                                                  
+(defadvice windmove-down (before other-window-now activate)                                                                                                                                               
+  (when buffer-file-name (save-buffer)))                                                                                                                                                                  
+(defadvice windmove-left (before other-window-now activate)                                                                                                                                               
+  (when buffer-file-name (save-buffer)))                                                                                                                                                                  
+(defadvice windmove-right (before other-window-now activate)                                                                                                                                              
+  (when buffer-file-name (save-buffer))) 
+
 ;; Key bindings
 
 ;; Misc:
