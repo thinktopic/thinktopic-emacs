5ad5fb7f81cd4a6ce0a694f850c8864fdc4dbfec
Jake Pearson <jpearson@rallydev.com>
2013-04-05 08:07:43 -0600

    C-c d will show a list of recent files in the mini buffer with ido search

--- init.el
+++ init.el
@@ -523,3 +523,18 @@
   (when (file-exists-p user-init)
     (load-file user-init)))
 
+
+;; Find recent files
+(require 'recentf)
+(setq recentf-max-saved-items 200
+      recentf-max-menu-items 15)
+(recentf-mode +1)
+
+(defun recentf-ido-find-file ()
+  "Find a recent file using ido."
+  (interactive)
+  (let ((file (ido-completing-read "Choose recent file: " recentf-list nil t)))
+    (when file
+      (find-file file))))
+
+(global-set-key (kbd "C-c d")  'recentf-ido-find-file)
