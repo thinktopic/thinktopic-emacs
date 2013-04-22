6c48f34f8797355cf62c7f9a8aaa1c494dd75227
Jake Pearson <jpearson@rallydev.com>
2013-04-21 21:16:25 -0600

    Shift+Up will now move to the window above the current one

--- init.el
+++ init.el
@@ -454,8 +454,10 @@
 
 ;; Window management - windmove to switch, windsize to resize
 (require 'windsize)
+(global-unset-key (kbd "<select>"))
 (windmove-default-keybindings) ;; Shift+direction
 (windsize-default-keybindings) ;; C-S+direction
+(global-set-key (kbd "<select>") 'windmove-up)
 
 ;; Multiple cursors (mostly so the Sublime junkies can't be too smug)
 (require 'multiple-cursors)
