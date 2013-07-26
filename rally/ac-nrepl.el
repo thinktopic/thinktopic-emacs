d15efc4c1f667efd53dda7b7d75e8181932f1f5e
Bob Cotton <bob.cotton@gmail.com>
2013-07-26 08:57:59 -0600

    Trying to get ac-nrepl working with doc popups

--- rally/ac-nrepl.el
+++ rally/ac-nrepl.el
@@ -1,3 +1,8 @@
 ;;; Autocomplete
 (require 'auto-complete)
 (global-auto-complete-mode)
+
+(eval-after-load "auto-complete"
+  '(add-to-list 'ac-modes 'nrepl-mode 'nrepl-interaction-mode))
+(add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
+(add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
