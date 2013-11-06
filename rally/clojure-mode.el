c481dd317cf96c151bdf66e3c0447c290920fb46
Chris Perkins <cperkins@rallydev.com>
2013-11-06 06:50:37 -0700

    Open .edn files in clojure-mode.

--- rally/clojure-mode.el
+++ rally/clojure-mode.el
@@ -4,3 +4,5 @@
              (require 'align-cljlet)
              (define-key clojure-mode-map (kbd "C-M-q") 'align-cljlet)
              (define-key clojure-mode-map (kbd "RET") 'paredit-newline)))
+
+(add-to-list 'auto-mode-alist '("\\.edn$" . clojure-mode))
