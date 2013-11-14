b2359774bc5146afe90903af41a8ee23bc4613f7
Chris Perkins <cperkins@rallydev.com>
2013-11-14 05:57:38 -0700

    Move crazy key-chords into jakify.

--- rally/key-chord.el
+++ rally/key-chord.el
@@ -5,15 +5,16 @@
     (interactive)
     (switch-to-buffer (other-buffer (current-buffer) 1)))
 
-(key-chord-define-global "yy" 'beginning-of-buffer)
-(key-chord-define-global "nn" 'end-of-buffer)
-(key-chord-define-global "JJ" 'switch-to-previous-buffer)
-(key-chord-define-global "jj" 'ace-jump-mode)
-(key-chord-define-global "ff" 'projectile-find-file)
-(key-chord-define-global "gg" 'goto-line)
-
 (defun jakify ()
   (interactive)
+
+  (key-chord-define-global "yy" 'beginning-of-buffer)
+  (key-chord-define-global "nn" 'end-of-buffer)
+  (key-chord-define-global "JJ" 'switch-to-previous-buffer)
+  (key-chord-define-global "jj" 'ace-jump-mode)
+  (key-chord-define-global "ff" 'projectile-find-file)
+  (key-chord-define-global "gg" 'goto-line)
   (key-chord-mode +1)
+
   (color-theme-cyberpunk)
   (global-rainbow-delimiters-mode))
