6fa2e1068426d2592facf1b6450a69cda263130f
Chris Perkins <cperkins@rallydev.com>
2013-11-16 15:52:16 -0700

    Patch for bug in clojure-mode+cider: C-c C-, was not printing test results.

--- rally/cider.el
+++ rally/cider.el
@@ -23,3 +23,12 @@
 (defadvice save-buffer (after cider-reload-saved-file activate)
   (cider-auto-reload))
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; The following bits fix up some stuff that is busted in cider
+;;; because it's very much in-development.
+
+;;; This one is fixed in HEAD of clojure-mode, as of 2013-11-16, but
+;;; not yet deployed to melpa:
+(defun nrepl-emit-interactive-output (value)
+  (cider-emit-interactive-output value))
