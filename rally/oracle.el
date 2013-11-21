e4fc8e6539c804603981b1c9b3de368d97092870
Chris Perkins <cperkins@rallydev.com>
2013-11-21 10:09:30 -0700

    Make is easier to connect to different Oracle databases in alm-db-connect.
    The prompt for "Connection" now allows you to enter a short-form:
    currently "eng" (the default) and "test" are supported.

--- rally/oracle.el
+++ rally/oracle.el
@@ -8,8 +8,8 @@
 
 (eval-after-load 'sqlplus '(tweak-sqlplus))
 
-(defvar oracle-dbs '((eng "dsc-engdb-02.f4tech.com:1530/okidb11")
-                     (test "??? fixme")))
+(defvar oracle-dbs '(("eng"  "dsc-engdb-02.f4tech.com:1530/okidb11")
+                     ("test" "bld-engdb-01.f4tech.com:1530/okiee11")))
 
 (defvar alm-username-history nil)
 (defvar alm-connection-history nil)
@@ -17,7 +17,7 @@
 (defun alm-db-connect ()
   (interactive)
   (let* ((hostname (first (split-string system-name "\\.")))
-         (conn     (second (assoc 'eng oracle-dbs)))
+         (conn     (second (assoc "eng" oracle-dbs)))
          (username (read-string (format "Username [default %s]: " hostname)
                                 nil
                                 'alm-username-history
@@ -29,6 +29,8 @@
                                   nil
                                   'alm-connection-history
                                   conn))
+         (connection (second (or (assoc connection oracle-dbs)
+                                 (list nil connection))))
          (connection-string (format "%s/%s@//%s" username password connection)))
     (sqlplus connection-string
              (concat "*" username "-input*")
