7dde96b4feab520f23e1dd2977b001f53784b584
jakepearson <jpearson@rallydev.com>
2014-01-27 07:18:14 -0700

    Enable yassnippet when in clj-refactor/enable global-auto-complete-mode

--- rally/refactor.el
+++ rally/refactor.el
@@ -1,4 +1,7 @@
 (require 'clj-refactor)
 (add-hook 'clojure-mode-hook (lambda ()
                                (clj-refactor-mode 1)
+                               (lambda () (yas/minor-mode 1))
                                (cljr-add-keybindings-with-prefix "C-c m")))
+
+(global-auto-complete-mode 1)
