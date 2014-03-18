ab211897628c284f9d9cfd67606d39648cf2d526
Chris Perkins <cperkins@rallydev.com>
2014-03-18 12:46:36 -0600

    Fix C-y inserting "blah blah [gemname]" crap.
    It was caused by RVM somehow. I'm not exactly sure how, but this seems
    to fix it.

--- rally/zsh.el
+++ rally/zsh.el
@@ -1,7 +1,12 @@
 ;; http://stackoverflow.com/questions/9435019/how-do-i-source-my-zshrc-within-emacs
-(let ((path (shell-command-to-string ". ~/.zshrc; echo -n $PATH")))
-  (setenv "PATH" path)
-  (setq exec-path
-        (append
-         (split-string-and-unquote path ":")
-         exec-path)))
+(defun setup-path-from-zsh ()
+  (shell-command-to-string ". ~/.zshrc")
+  (let ((path (shell-command-to-string "echo -n $PATH")))
+    (setenv "PATH" path)
+    (setq exec-path
+          (append
+           (split-string-and-unquote path ":")
+           exec-path))))
+
+(setup-path-from-zsh)
+
