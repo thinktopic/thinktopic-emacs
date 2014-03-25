5bfe4b083231f1ed4b00a045c14db15f9595df56
Adam Esterline <adam@esterlines.com>
2014-03-25 09:06:24 -0600

    Fixing some dependency errors.

--- profiles.clj
+++ profiles.clj
@@ -1,15 +1,14 @@
 {:user
  {:dependencies
   [[org.clojars.gjahad/debug-repl "0.3.3"]
-   [leiningen "2.3.4"]
    [im.chit/vinyasa "0.1.8"]
-   [spyscope "0.1.4"]
-   [lein-ancient "0.5.5"]
-   [jonase/eastwood "0.1.1"]]
+   [spyscope "0.1.4"]]
+  :plugins [[lein-ancient "0.5.5"]
+            [jonase/eastwood "0.1.1"]]
   :injections [(require 'vinyasa.inject)
                (require 'spyscope.core)
                (require 'alex-and-georges.debug-repl)
                (vinyasa.inject/inject 'clojure.core '>
                                       '[[clojure.repl doc source]
                                         [clojure.pprint pprint pp]
-                                        [alex-and-georges.debug-repl debug-repl]])]}}}}
+                                        [alex-and-georges.debug-repl debug-repl]])]}}
