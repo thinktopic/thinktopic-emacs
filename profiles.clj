7c6b027d05131bb8bfacdc48e7b2e63e75056894
jakepearson <jpearson@rallydev.com>
2014-04-28 07:48:50 -0600

    Added cider-nrepl plugin

--- profiles.clj
+++ profiles.clj
@@ -1,10 +1,18 @@
 {:user
  {:plugins [[lein-ancient "0.5.5"]
-            [jonase/eastwood "0.1.1"]]}
+            [jonase/eastwood "0.1.1"]
+            [rally-service/lein-template "0.1.0-SNAPSHOT"]
+            [cider/cider-nrepl "0.7.0-SNAPSHOT"]]}
  :repl
  {:dependencies [[org.clojars.gjahad/debug-repl "0.3.3"]
                  [im.chit/vinyasa "0.1.8"]
                  [spyscope "0.1.4"]]
+  :repl-options {:nrepl-middleware
+                 [cider.nrepl.middleware.classpath/wrap-classpath
+                  cider.nrepl.middleware.complete/wrap-complete
+                  cider.nrepl.middleware.info/wrap-info
+                  cider.nrepl.middleware.inspect/wrap-inspect
+                  cider.nrepl.middleware.stacktrace/wrap-stacktrace]}
   :injections [(require 'vinyasa.inject)
                (require 'spyscope.core)
                (require 'alex-and-georges.debug-repl)
