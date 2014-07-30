97eda60177f979528afb5c0966b2938753cbd89d
jakepearson <jpearson@rallydev.com>
2014-07-30 14:23:57 -0600

    ALM Docker Instance

---
+++ docker/alm/resources/java_plugin.zsh
@@ -0,0 +1,4 @@
+export JAVA6_HOME=/usr/lib/jvm/java-7-oracle/
+export JAVA7_HOME=/usr/lib/jvm/java-7-oracle/
+export JAVA8_HOME=/usr/lib/jvm/java-8-oracle/
+export JAVA_HOME=/usr/lib/jvm/java-8-oracle/
