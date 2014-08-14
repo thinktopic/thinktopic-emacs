bd275a9e26b463745d58a54484c448b4511cc56d
Chris Perkins <cperkins@rallydev.com>
2014-08-14 17:13:32 -0600

    Updated profile.clj. Notably: latest cider-nrepl.
    If you update your ~/.lein/profiles.clj from this, you should also
    update your cider package in emacs (M-x package-list-packages, then U,
    then follow the prompts).
    
      Please enter the commit message for your changes. Lines starting
      with '#' will be ignored, and an empty message aborts the commit.
      On branch master
      Changes to be committed:
        (use "git reset HEAD <file>..." to unstage)
    
     	modified:   profiles.clj
    
      Changes not staged for commit:
        (use "git add <file>..." to update what will be committed)
        (use "git checkout -- <file>..." to discard changes in working directory)
    
     	modified:   tmux.conf.linux
     	modified:   tmux.conf.osx

--- profiles.clj
+++ profiles.clj
@@ -1,21 +1,15 @@
 {:user
  {:plugins [[lein-ancient "0.5.5"]
-            [jonase/eastwood "0.1.1"]
-            [cider/cider-nrepl "0.7.0-SNAPSHOT"]]}
+            [jonase/eastwood "0.1.4"]
+            [cider/cider-nrepl "0.8.0-SNAPSHOT"]]}
  :repl
  {:dependencies [[org.clojars.gjahad/debug-repl "0.3.3"]
-                 [org.clojure/tools.namespace "0.2.4"]
+                 [org.clojure/tools.namespace "0.2.5"]
                  [im.chit/vinyasa "0.1.8"]
                  [spyscope "0.1.4"]
                  ;[blackwater "0.0.9"]
                  [clj-time "0.6.0"]
                  [print-foo "0.5.0"]]
-  :repl-options {:nrepl-middleware
-                 [cider.nrepl.middleware.classpath/wrap-classpath
-                  cider.nrepl.middleware.complete/wrap-complete
-                  cider.nrepl.middleware.info/wrap-info
-                  cider.nrepl.middleware.inspect/wrap-inspect
-                  cider.nrepl.middleware.stacktrace/wrap-stacktrace]}
   :injections [(load-file (.getAbsolutePath (clojure.java.io/file (System/getenv "HOME") ".lein/user.clj")))
                (require 'vinyasa.inject)
                (require 'spyscope.core)
@@ -26,7 +20,7 @@
                                       '[[clojure.repl doc source]
                                         [clojure.pprint pprint pp]
                                         [alex-and-georges.debug-repl debug-repl]
-                                        ;[clojure.tools.names.repl refresh]
+                                        [clojure.tools.namespace.repl refresh]
                                         ;[black.water.jdbc decorate-cjj!]
                                         [user run-test spy]
                                         ])]}}
