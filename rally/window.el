a3597c3a358b20efbd34b1df27bea755b9571f57
jakepearson <jpearson@rallydev.com>
2014-12-16 19:39:33 -0700

    Do not create windows automatically

--- rally/window.el
+++ rally/window.el
@@ -2,3 +2,6 @@
 (global-unset-key (kbd "<select>"))
 (windmove-default-keybindings) ;; Shift+direction
 (global-set-key (kbd "<select>") 'windmove-up)
+
+(setq split-height-threshold nil)
+(setq split-width-threshold nil)
