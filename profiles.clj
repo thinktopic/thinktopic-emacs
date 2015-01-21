5d95c5d82aae4ac0a69da05579b3250509b81e2a
Chris Perkins <cperkins@rallydev.com>
2015-01-20 17:32:20 -0700

    Update versions in profiles.clj

--- profiles.clj
+++ profiles.clj
@@ -1,26 +1,23 @@
 {:user
  {:plugins [[lein-ancient "0.5.5"]
-            [jonase/eastwood "0.1.4"]
+            [jonase/eastwood "0.2.1"]
             [cider/cider-nrepl "0.9.0-SNAPSHOT"]]}
  :repl
  {:dependencies [[org.clojars.gjahad/debug-repl "0.3.3"]
-                 [org.clojure/tools.namespace "0.2.5"]
-                 [im.chit/vinyasa "0.1.8"]
-                 [spyscope "0.1.4"]
-                 ;[blackwater "0.0.9"]
-                 [clj-time "0.6.0"]
-                 [print-foo "0.5.0"]]
+                 [org.clojure/tools.namespace "0.2.8"]
+                 [im.chit/vinyasa "0.2.0"]
+                 [spyscope "0.1.5"]
+                 [print-foo "1.0.1"]
+                 ]
   :injections [(load-file (.getAbsolutePath (clojure.java.io/file (System/getenv "HOME") ".lein/user.clj")))
                (require 'vinyasa.inject)
                (require 'spyscope.core)
                (require 'alex-and-georges.debug-repl)
                (use 'print.foo)
-               ;(require 'black.water.jdbc)
                (vinyasa.inject/inject 'clojure.core '>
                                       '[[clojure.repl doc source]
                                         [clojure.pprint pprint pp]
                                         [alex-and-georges.debug-repl debug-repl]
                                         [clojure.tools.namespace.repl refresh refresh-all]
-                                        ;[black.water.jdbc decorate-cjj!]
                                         [user run-test spy]
                                         ])]}}
