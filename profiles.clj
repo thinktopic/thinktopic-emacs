497578dcb7fd7549b0186d76c2254f28b49bb51f
Chris Perkins <cperkins@rallydev.com>
2015-03-16 14:05:20 -0600

    Move some of the stuff in profiles.clj into :repl profile.
    Having cider-nrepl in the user profile causes it to be loaded when it's
    not needed (eg: "lein run"), which was causing version conflicts in some
    places - notably in Zuul.

--- profiles.clj
+++ profiles.clj
@@ -1,24 +1,22 @@
 {:user
  {:plugins [[lein-ancient "0.6.2"]
-            [jonase/eastwood "0.2.1"]
-            [cider/cider-nrepl "0.9.0-SNAPSHOT"]
-            [refactor-nrepl "0.2.2"]]}
+            [jonase/eastwood "0.2.1"]]}
  :repl
- {:dependencies [[org.clojars.gjahad/debug-repl "0.3.3"]
-                 [org.clojure/tools.namespace "0.2.8"]
+ {:plugins      [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
+                 [refactor-nrepl "0.2.2"]]
+  :dependencies [[org.clojure/tools.nrepl "0.2.7"]
+                 [org.clojars.gjahad/debug-repl "0.3.3"]
+                 [org.clojure/tools.namespace "0.2.10"]
                  [im.chit/vinyasa "0.2.0"]
-                 [spyscope "0.1.5"]
-                 ;;[print-foo "1.0.1"]
-                 ]
-  :injections [(load-file (.getAbsolutePath (clojure.java.io/file (System/getenv "HOME") ".lein/user.clj")))
-               (require 'vinyasa.inject)
-               (require 'spyscope.core)
-               (require 'alex-and-georges.debug-repl)
-               ;;(use 'print.foo)
-               (vinyasa.inject/inject 'clojure.core '>
-                                      '[[clojure.repl doc source pst]
-                                        [clojure.pprint pprint pp]
-                                        [alex-and-georges.debug-repl debug-repl]
-                                        [clojure.tools.namespace.repl refresh refresh-all]
-                                        [user run-test spy]
-                                        ])]}}
+                 [spyscope "0.1.5"]]
+  :injections   [(load-file (.getAbsolutePath (clojure.java.io/file (System/getenv "HOME") ".lein/user.clj")))
+                 (require 'vinyasa.inject)
+                 (require 'spyscope.core)
+                 (require 'alex-and-georges.debug-repl)
+                 (vinyasa.inject/inject 'clojure.core '>
+                                        '[[clojure.repl doc source pst]
+                                          [clojure.pprint pprint pp]
+                                          [alex-and-georges.debug-repl debug-repl]
+                                          [clojure.tools.namespace.repl refresh refresh-all]
+                                          [user run-test spy]
+                                          ])]}}
