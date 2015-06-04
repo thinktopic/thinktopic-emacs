78b8ef79fee824d6470c4fce7665197b5c8e5acf
Chris Perkins <cperkins@rallydev.com>
2015-06-04 09:22:43 -0600

    Tune defaults for window splitting.

--- rally/window.el
+++ rally/window.el
@@ -2,3 +2,12 @@
 (global-unset-key (kbd "<select>"))
 (windmove-default-keybindings) ;; Shift+direction
 (global-set-key (kbd "<select>") 'windmove-up)
+
+
+;;; Never split into top/bottom (nobody wants that):
+(setq split-height-threshold nil)
+
+;;; Tune splitting for the larger monitors that most people use these
+;;; days The emacs default, which splits at a width of 160, is not
+;;; ideal.
+(setq split-width-threshold 200)
