4448169f9690cb4f095241320dc0f06fa07ab8d4
Chris Perkins <cperkins@rallydev.com>
2015-07-23 13:21:24 -0600

    Linux like to paste too.

--- rally/clipboard.el
+++ rally/clipboard.el
@@ -7,5 +7,6 @@
       (process-send-string proc text)
       (process-send-eof proc))))
 
-(setq interprogram-cut-function 'paste-to-osx)
-(setq interprogram-paste-function 'copy-from-osx)
+(when (eq system-type 'darwin)
+  (setq interprogram-cut-function 'paste-to-osx)
+  (setq interprogram-paste-function 'copy-from-osx))
