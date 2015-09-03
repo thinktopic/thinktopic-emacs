b5337719298a0c48e6bddc7108a4ea1fba1bdd42
Chris Perkins <cperkins@rallydev.com>
2015-09-03 11:05:52 -0600

    Add terminal-friendly bindings for multiple-cursors

--- rally/multiple-cursors.el
+++ rally/multiple-cursors.el
@@ -1,6 +1,14 @@
 ;; Multiple cursors (mostly so the Sublime junkies can't be too smug)
 (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+(global-set-key (kbd "C->")         'mc/mark-next-like-this)
+(global-set-key (kbd "C-<")         'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<")     'mc/mark-all-like-this)
 
+
+;;; bindings that work in terminal
+
+(global-set-key (kbd "C-x M-'") 'mc/edit-lines)
+(global-set-key (kbd "M-+")     'mc/mark-next-like-this)
+(global-set-key (kbd "M-_")     'mc/mark-previous-like-this)
+(global-set-key (kbd "C-x M-+") 'mc/mark-all-like-this)
+(global-set-key (kbd "C-x _")   'mc-hide-unmatched-lines-mode)
