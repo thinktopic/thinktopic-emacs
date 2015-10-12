7075aa81b2a3a5a91265f39fb35125c1bf592d07
Chris Perkins and Neeraj Khadakkar <cperkins@rallydev.com>
2015-10-12 09:47:23 -0600

    Update clj-refactor

--- profiles.clj
+++ profiles.clj
@@ -1,7 +1,7 @@
 {:user
  {:plugins      [[lein-ancient "0.6.7"]
                  [cider/cider-nrepl "0.10.0-SNAPSHOT"]
-                 [refactor-nrepl "1.2.0-SNAPSHOT"]
+                 [refactor-nrepl "2.0.0-SNAPSHOT"]
                  [quickie "0.4.0"]]
   :dependencies [[org.clojure/tools.nrepl "0.2.10"]]
   
