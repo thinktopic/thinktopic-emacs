1ef1371d5b36c634bafac0b2890d9026b226ea05
Chris Perkins <chrisperkins99@gmail.com>
2015-11-19 15:15:56 -0700

    Adjust profiles.clj
    Moved cider-nrepl stuff in to :repl profile, which should allow other,
    non-repl tasks to start up faster.
    
    Seems to also have the effect of fixing some errors when running lein in
    zuul, although I'm not sure why.

--- profiles.clj
+++ profiles.clj
@@ -1,16 +1,14 @@
 {:user
  {:plugins      [[lein-ancient "0.6.7"]
-                 [cider/cider-nrepl "0.10.0-SNAPSHOT"]
-                 [refactor-nrepl "2.0.0-SNAPSHOT"]
                  [quickie "0.4.0"]]
-  :dependencies [[org.clojure/tools.nrepl "0.2.12"]]
-  
   :repl-options
   {:init (do (load-file (.getAbsolutePath (clojure.java.io/file (System/getenv "HOME") ".lein/user.clj")))
              (user/inject-repl-utils))}}
  
  :repl
- {:dependencies [[org.clojars.gjahad/debug-repl "0.3.3"]
-                 [org.clojure/tools.namespace "0.2.10"]
+ {:plugins      [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
+                 [refactor-nrepl "2.0.0-SNAPSHOT"]]
+  :dependencies [[org.clojars.gjahad/debug-repl "0.3.3"]
+                 [org.clojure/tools.namespace "0.2.11"]
                  [im.chit/vinyasa "0.3.4"]
                  [spyscope "0.1.5"]]}}
