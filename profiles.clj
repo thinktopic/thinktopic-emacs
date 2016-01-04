1bcc660aa24b1629edd5040a0ce888a005a7147f
Joshua Clark <joshuaegclark@gmail.com>
2016-01-04 12:01:59 -0700

    Update dependencies. cider -> 0.11.0-SNAPSHOT

--- profiles.clj
+++ profiles.clj
@@ -1,12 +1,12 @@
 {:user
- {:plugins      [[lein-ancient "0.6.7"]
-                 [quickie "0.4.0"]]
+ {:plugins      [[lein-ancient "0.6.8"]
+                 [quickie "0.4.1"]]
   :repl-options
   {:init (do (load-file (.getAbsolutePath (clojure.java.io/file (System/getenv "HOME") ".lein/user.clj")))
              (user/inject-repl-utils))}}
  
  :repl
- {:plugins      [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
+ {:plugins      [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
                  [refactor-nrepl "2.0.0-SNAPSHOT"]]
   :dependencies [[org.clojars.gjahad/debug-repl "0.3.3"]
                  [org.clojure/tools.namespace "0.2.11"]
