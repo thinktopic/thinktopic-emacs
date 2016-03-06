723063dfd5f868d2fe0eb41d74c83d326b0c0653
Chris Perkins <chrisperkins99@gmail.com>
2016-03-06 11:56:15 -0700

    Bump cider-nrepl version to 0.12.0-SNAPSHOT

--- profiles.clj
+++ profiles.clj
@@ -7,7 +7,7 @@
              (user/inject-repl-utils))}}
  
  :repl
- {:plugins      [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
+ {:plugins      [[cider/cider-nrepl "0.12.0-SNAPSHOT"]
                  [refactor-nrepl "2.2.0-SNAPSHOT"]]
   :dependencies [[org.clojars.gjahad/debug-repl "0.3.3"]
                  [org.clojure/tools.namespace "0.2.11"]
